8f7fd11d7fe91c11120b6e8767e7884f8d283407
DupuyT <taylor.dupuy@colorado.edu>
2022-12-01 10:52:32 -0700

    In Class Changes

--- the-app/src/FootSteps.js
+++ the-app/src/FootSteps.js
@@ -2,12 +2,18 @@ import React, { PureComponent } from "react";
 import PropTypes from "prop-types";
 
 export default class FootSteps extends PureComponent {
+
+  openInNewTab = url => {
+    window.open(url, '_blank', 'noopener,noreferrer');
+  };
+  
   render() {
     const { name } = this.props;
     const { number } = this.props;
+    const { url } = this.props;
     const cls = `footsteps footsteps-${number}`;
     return (
-      <div className={cls}>
+      <div className={cls} onClick={() => this.openInNewTab(url)}>
         <div className="footstep left" />
         <div className="footstep right" />
         <div className="scroll-name">
@@ -20,5 +26,6 @@ export default class FootSteps extends PureComponent {
 
 FootSteps.propTypes = {
   name: PropTypes.string.isRequired,
-  number: PropTypes.number.isRequired
+  number: PropTypes.number.isRequired,
+  url: PropTypes.string.isRequired
 };
