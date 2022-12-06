c19069f385638636ece70d2805b77a367948cb01
JHamilton-Burns <jeha2598@colorado.edu>
2022-12-06 09:40:38 -0700

    Updated Links

--- the-app/src/App.js
+++ the-app/src/App.js
@@ -42,3 +42,59 @@ export default class App extends React.Component {
     );
   }
 }
+
+//           <div onClick={this.toggleMap}>
+//             <div className={this.state.active ? "map-floor2 active" : "map-floor2"}>
+//               <FootSteps name="Peter" number={1} url = "https://www.colorado.edu/atlas/peter-rosenthal" />
+//               <FootSteps name="Dr.Z" number={2} url = "https://www.colorado.edu/atlas/shaz-zamore" />
+//               <MapFlap number={1} />
+//               <MapFlap number={2} />
+//               <MapSide side={1} map={83} isback={true} />
+//               <MapSide side={2} map={183} isback={true} />
+//               <div onClick={this.toggleMap}>
+//               <MapSide side={3} map={7} isback={true} />
+//               <MapSide side={4} map={10} isback={false} />
+//               <MapSide side={4} map={10} isback={false} />
+//               <MapSide side={5} map={6} isback={true} />
+//               <MapSide side={6} map={11} isback={true} />
+//               </div>
+//             </div>
+//           </div>
+
+//             <div onClick={this.toggleMap}>
+//               <div className={this.state.active ? "map-b2 active" : "map-b2"}>
+//                 <FootSteps name="Peter" number={1} url = "https://www.colorado.edu/atlas/peter-rosenthal" />
+//                 <FootSteps name="Dr.Z" number={2} url = "https://www.colorado.edu/atlas/shaz-zamore" />
+//                 <MapFlap number={1} />
+//                 <MapFlap number={2} />
+//                 <MapSide side={1} map={81} isback={true} />
+//                 <MapSide side={2} map={181} isback={true} />
+//                 <div onClick={this.toggleMap}>
+//                 <MapSide side={3} map={7} isback={true} />
+//                 <MapSide side={4} map={10} isback={false} />
+//                 <MapSide side={4} map={10} isback={false} />
+//                 <MapSide side={5} map={6} isback={true} />
+//                 <MapSide side={6} map={11} isback={true} />
+//                 </div>
+//               </div>
+//             </div>
+
+//           <div onClick={this.toggleMap}>
+//             <div className={this.state.active ? "map-b1 active" : "map-b1"}>
+//               <FootSteps name="Peter" number={1} url = "https://www.colorado.edu/atlas/peter-rosenthal" />
+//               <FootSteps name="Dr.Z" number={2} url = "https://www.colorado.edu/atlas/shaz-zamore" />
+//               <MapFlap number={1} />
+//               <MapFlap number={2} />
+//               <MapSide side={1} map={82} isback={true} />
+//               <MapSide side={2} map={182} isback={true} />
+//               <div onClick={this.toggleMap}>
+//               <MapSide side={3} map={7} isback={true} />
+//               <MapSide side={4} map={10} isback={false} />
+//               <MapSide side={4} map={10} isback={false} />
+//               <MapSide side={5} map={6} isback={true} />
+//               <MapSide side={6} map={11} isback={true} />
+//               </div>
+//             </div>
+//           <div onClick={this.toggleMap}>
+
+// 
